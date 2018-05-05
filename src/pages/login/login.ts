405787c1c3f66ae746e3ae0c96cc9d4bbf56734d
Pedro <pedropacheco92@outlook.com>
2018-05-05 19:19:39 -0300

    app sem setic

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -36,24 +36,24 @@ export class LoginPage {
     private deeplinks: Deeplinks,
     private plataform: Platform) {
 
-    this.deeplinks.routeWithNavController(this.navCtrl, {
-      '*': LoginPage
-    }).subscribe((match) => {
-      this.browser.close();
-      // match.$route - the route we matched, which is the matched entry from the arguments to route()
-      // match.$args - the args passed in the link
-      // match.$link - the full link data
-      let code = (match.$link['queryString']);
-      code = code.replace('code=', "");
-      code = code.replace('&state=E3ZYKC1T6H2yP4z', '');     
-      this.loginProvider.authSetic(code)
-        .map(res => res.json())
-        .subscribe(res => {
-          this.doLogin(res['access_token']);
-        }, err => alert(err));
-    }, (nomatch) => {
-      console.error('Got a deeplink that didn\'t match', nomatch);
-    });
+    // this.deeplinks.routeWithNavController(this.navCtrl, {
+    //   '*': LoginPage
+    // }).subscribe((match) => {
+    //   this.browser.close();
+    //   // match.$route - the route we matched, which is the matched entry from the arguments to route()
+    //   // match.$args - the args passed in the link
+    //   // match.$link - the full link data
+    //   let code = (match.$link['queryString']);
+    //   code = code.replace('code=', "");
+    //   code = code.replace('&state=E3ZYKC1T6H2yP4z', '');     
+    //   this.loginProvider.authSetic(code)
+    //     .map(res => res.json())
+    //     .subscribe(res => {
+    //       this.doLogin(res['access_token']);
+    //     }, err => alert(err));
+    // }, (nomatch) => {
+    //   console.error('Got a deeplink that didn\'t match', nomatch);
+    // });
 
     this.translateService.setDefaultLang('pt');
 
@@ -90,20 +90,20 @@ export class LoginPage {
   }
 
   prepareLogin() {
-    let api_key = 'oauth';
+    // let api_key = 'oauth';
     // let api_key = 'tccphpils';
-    let secret_key = 'segredo';
+    // let secret_key = 'segredo';
     // let secret_key = 'Gto1W3ErSqWWdmASpb5CsqqPkgjNv8';
-    let redirect_url = 'ufsclogin://setic_oauth_example.ufsc.br';
+    // let redirect_url = 'ufsclogin://setic_oauth_example.ufsc.br';
     // let redirect_url = 'tccphpils://tccphpils.setic_oauth.ufsc.br';
-    let site = 'https://sistemas.homologacao.ufsc.br/oauth2.0/';
+    // let site = 'https://sistemas.homologacao.ufsc.br/oauth2.0/';
     // let site = 'https://sistemas.ufsc.br/oauth2.0/';
 
-    let url = site + 'authorize?client_id='+ api_key +'&client_secret='+ secret_key +'&redirect_uri=' + redirect_url + '&state=E3ZYKC1T6H2yP4z&response_type=code&bypass_approval_prompt=true'; //&bypass_approval_prompt=true
-    this.browser = this.iab.create(url, '_system', { location: 'yes'});
+    // let url = site + 'authorize?client_id='+ api_key +'&client_secret='+ secret_key +'&redirect_uri=' + redirect_url + '&state=E3ZYKC1T6H2yP4z&response_type=code&bypass_approval_prompt=true'; //&bypass_approval_prompt=true
+    // this.browser = this.iab.create(url, '_system', { location: 'yes'});
     
-    this.browser.close();
-    // this.doLogin("AT-10-XSLAp9Ec0eEHo02aMoUhBzpdU66bTGmoNYO");
+    // this.browser.close();
+    this.doLogin("AT-10-XSLAp9Ec0eEHo02aMoUhBzpdU66bTGmoNYO");
   }
 
   doLogin(token: string) {
