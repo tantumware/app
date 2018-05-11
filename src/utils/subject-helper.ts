d0635c0497b12d2360f4c3348f8d3e6dcd7fdc31
Pedro <pedropacheco92@outlook.com>
2018-05-11 09:29:39 -0300

    next subjects

--- src/utils/subject-helper.ts
+++ src/utils/subject-helper.ts
@@ -27,18 +27,36 @@ export class SubjectHelper {
         let newSubjects: Subject[] = [];
         let day = new Date().getDay() + 1;
         day = day > 7 ? 2 : day;
+        let highDay = 0;
+        let lowDay = 99;
+
+        for (let i in subjects) {
+            let subject: Subject = subjects[i]
+            for (let h in subject.horarios) {
+                let time = Number(subject.horarios[h].substring(0, 1));
+                if (time > highDay) {
+                    highDay = time;
+                }
+                if (time < lowDay) {
+                    lowDay = time;
+                }
+            }
+        }
 
         for (let i in subjects) {
             let subject: Subject = subjects[i]
             for (let h in subject.horarios) {
                 let time = subject.horarios[h];
+                let currentDay =  Number(subject.horarios[h].substring(0, 1));
+                if (day >= highDay && currentDay == lowDay) {
+                    newSubjects.push(this.subject(subject, time));                    
+                }
                 if (newSubjects.length == 2) {
                     return newSubjects
                 }
-                newSubjects.push(this.subject(subject, time));
+                // newSubjects.push(this.subject(subject, time));
             }
         }
-
         return newSubjects;
     }
 
